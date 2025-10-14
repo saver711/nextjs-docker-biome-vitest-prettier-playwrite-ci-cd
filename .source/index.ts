6bad38f0e99809cd281eea0ba0442a785c76c988
Ramy <ramyalsabry.dev@gmail.com>
2025-10-14 22:27:32 +0300

    feat(VOLT-01): implement dialog

--- .source/index.ts
+++ .source/index.ts
@@ -1,60 +1,68 @@
 // @ts-nocheck -- skip type checking
 
 import { _runtime } from "fumadocs-mdx/runtime/next";
-import * as docs_1 from "../content/docs/components/index.mdx?collection=docs&hash=1759691348746";
-import * as docs_2 from "../content/docs/components/table.mdx?collection=docs&hash=1759691348746";
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1759691348746";
-import * as docs_3 from "../content/docs/temp-dir/components.mdx?collection=docs&hash=1759691348746";
-import * as docs_4 from "../content/docs/temp-dir/index.mdx?collection=docs&hash=1759691348746";
-import * as docs_5 from "../content/docs/temp-dir/subpage.mdx?collection=docs&hash=1759691348746";
+import * as docs_1 from "../content/docs/components/dialog.mdx?collection=docs&hash=1760469221149";
+import * as docs_2 from "../content/docs/components/index.mdx?collection=docs&hash=1760469221149";
+import * as docs_3 from "../content/docs/components/table.mdx?collection=docs&hash=1760469221149";
+import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1760469221149";
+import * as docs_4 from "../content/docs/temp-dir/components.mdx?collection=docs&hash=1760469221149";
+import * as docs_5 from "../content/docs/temp-dir/index.mdx?collection=docs&hash=1760469221149";
+import * as docs_6 from "../content/docs/temp-dir/subpage.mdx?collection=docs&hash=1760469221149";
 import type * as _source from "../source.config.mjs";
 export const docs = _runtime.docs<typeof _source.docs>(
 	[
 		{
-			info: { path: "index.mdx", fullPath: "content/docs/index.mdx" },
+			info: { path: "index.mdx", fullPath: "content\\docs\\index.mdx" },
 			data: docs_0,
 		},
 		{
 			info: {
-				path: "components/index.mdx",
-				fullPath: "content/docs/components/index.mdx",
+				path: "components/dialog.mdx",
+				fullPath: "content\\docs\\components\\dialog.mdx",
 			},
 			data: docs_1,
 		},
 		{
 			info: {
-				path: "components/table.mdx",
-				fullPath: "content/docs/components/table.mdx",
+				path: "components/index.mdx",
+				fullPath: "content\\docs\\components\\index.mdx",
 			},
 			data: docs_2,
 		},
 		{
 			info: {
-				path: "temp-dir/components.mdx",
-				fullPath: "content/docs/temp-dir/components.mdx",
+				path: "components/table.mdx",
+				fullPath: "content\\docs\\components\\table.mdx",
 			},
 			data: docs_3,
 		},
 		{
 			info: {
-				path: "temp-dir/index.mdx",
-				fullPath: "content/docs/temp-dir/index.mdx",
+				path: "temp-dir/components.mdx",
+				fullPath: "content\\docs\\temp-dir\\components.mdx",
 			},
 			data: docs_4,
 		},
 		{
 			info: {
-				path: "temp-dir/subpage.mdx",
-				fullPath: "content/docs/temp-dir/subpage.mdx",
+				path: "temp-dir/index.mdx",
+				fullPath: "content\\docs\\temp-dir\\index.mdx",
 			},
 			data: docs_5,
+		},
+		{
+			info: {
+				path: "temp-dir/subpage.mdx",
+				fullPath: "content\\docs\\temp-dir\\subpage.mdx",
+			},
+			data: docs_6,
 		},
 	],
 	[
 		{
 			info: {
 				path: "temp-dir/meta.json",
-				fullPath: "content/docs/temp-dir/meta.json",
+				fullPath: "content\\docs\\temp-dir\\meta.json",
 			},
 			data: {
 				pages: [
